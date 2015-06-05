506d3fdcf32766a68ccbc7ebd017628e254e52ab
Cole Rudnick <rudnickcole@gmail.com>
2015-06-04 18:30:40 -0700

    quick
    quick

--- src/GH/Main.java
+++ src/GH/Main.java
@@ -13,28 +13,30 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class Main implements ActionListener{
-	//objects
+public class Main implements ActionListener {
+	// objects
 	JFrame jf = new JFrame();
 	JPanel jp = new JPanel();
 	JButton jb = new JButton("Test");
-	
-public static void main(String[] args) throws IOException {
-	Main m = new Main();
-}
-public Main() throws IOException {
-	jf.add(jp);
-	jp.add(jb);
-	jf.setVisible(true);
-	jf.pack();
-	jb.addActionListener(this);
-	Image image = ImageIO.read(getClass().getResource("gh3.lor.07.lg.jpg"));
-	JLabel label = new JLabel(new ImageIcon(image));
-	jp.add(label);
-}
-@Override
-public void actionPerformed(ActionEvent e) {
-	JOptionPane.showMessageDialog(null, "Your test worked! Good job.");
-	
-}
+//t
+	public static void main(String[] args) throws IOException {
+		Main m = new Main();
+	}
+
+	public Main() throws IOException {
+		jf.add(jp);
+		jp.add(jb);
+		jf.setVisible(true);
+		jf.pack();
+		jb.addActionListener(this);
+		Image image = ImageIO.read(getClass().getResource("gh3.lor.07.lg.jpg"));
+		JLabel label = new JLabel(new ImageIcon(image));
+		jp.add(label);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(null, "Your test worked! Good job.");
+
+	}
 }
